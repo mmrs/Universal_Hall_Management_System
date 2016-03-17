d8f1d7f143f1f69e4d8a3c543366d0d807fda296
Moshiur Rahman <moshiur.siyam@gmail.com>
2016-03-17 20:47:56 +0600

    ui changes

--- MobileApp/UHMS/app/src/main/java/sust/uhms/MainActivity.java
+++ MobileApp/UHMS/app/src/main/java/sust/uhms/MainActivity.java
@@ -4,17 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.nhaarman.listviewanimations.appearance.simple.AlphaInAnimationAdapter;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ListView listView = (ListView) findViewById(R.id.listview);
-        listView.setAdapter(new ListViewAdapter(MainActivity.this));
-
-
+        ListViewAdapter listViewAdapter = new ListViewAdapter(MainActivity.this);
+        AlphaInAnimationAdapter alphaInAnimationAdapter = new AlphaInAnimationAdapter(listViewAdapter);
+        alphaInAnimationAdapter.setAbsListView(listView);
+        listView.setAdapter(alphaInAnimationAdapter);
 
     }
 }
